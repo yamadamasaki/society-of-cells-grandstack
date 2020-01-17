76804c59b0132bac5c1c089f7ca2e181f1ced322
YAMADA Masaki <masaki@metabolics.co.jp>
2020-01-17 15:36:17 +0900

    元の App.js 中のレイアウト部分を OriginalLayout.js として分離する

---
+++ ui/src/components/OriginalLayout.js
@@ -0,0 +1,224 @@
+import React, {Component} from "react";
+import {withStyles} from "@material-ui/core/styles";
+
+import {
+  AppBar,
+  CssBaseline,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  Typography
+} from "@material-ui/core";
+
+import {
+  ChevronLeft as ChevronLeftIcon,
+  Dashboard as DashboardIcon,
+  EventNote as EventNoteIcon,
+  Menu as MenuIcon,
+  People as PeopleIcon
+} from "@material-ui/icons";
+import classNames from "classnames";
+
+const drawerWidth = 240;
+
+const styles = theme => ({
+  root: {
+    display: "flex"
+  },
+  toolbar: {
+    paddingRight: 24
+  },
+  toolbarIcon: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: "0 8px",
+    ...theme.mixins.toolbar
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    backgroundColor: "#383838"
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  },
+  menuButton: {
+    marginLeft: 12,
+    marginRight: 36
+  },
+  menuButtonHidden: {
+    display: "none"
+  },
+  title: {
+    flexGrow: 1
+  },
+  drawerPaper: {
+    position: "relative",
+    whiteSpace: "nowrap",
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  },
+  drawerPaperClose: {
+    overflowX: "hidden",
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    width: theme.spacing(7),
+    [theme.breakpoints.up("sm")]: {
+      width: theme.spacing(9)
+    }
+  },
+  appBarSpacer: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    height: "100vh",
+    overflow: "auto"
+  },
+  chartContainer: {
+
+  }
+});
+
+class OriginalLayout extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedView: "Home",
+      open: true
+    };
+  }
+
+  setSelectedView(viewName) {
+    this.setState({
+      selectedView: viewName
+    });
+  }
+
+  handleDrawerOpen = () => {
+    this.setState({open: true});
+  };
+
+  handleDrawerClose = () => {
+    this.setState({open: false});
+  };
+
+  render() {
+    const {children, classes} = this.props;
+
+    return (
+      <React.Fragment>
+        <CssBaseline/>
+        <div className={classes.root}>
+          <AppBar
+            position="absolute"
+            className={classNames(
+              classes.appBar,
+              this.state.open && classes.appBarShift
+            )}
+          >
+            <Toolbar
+              disableGutters={!this.state.open}
+              className={classes.toolbar}
+            >
+              <IconButton
+                color="inherit"
+                aria-label="Open drawer"
+                onClick={this.handleDrawerOpen}
+                className={classNames(
+                  classes.menuButton,
+                  this.state.open && classes.menuButtonHidden
+                )}
+              >
+                <MenuIcon/>
+              </IconButton>
+              <Typography
+                component="h1"
+                variant="h2"
+                color="inherit"
+                noWrap
+                className={classes.title}
+              >
+                Welcome To GRANDstack
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <Drawer
+            variant="permanent"
+            classes={{
+              paper: classNames(
+                classes.drawerPaper,
+                !this.state.open && classes.drawerPaperClose
+              )
+            }}
+            open={this.state.open}
+          >
+            <div className={classes.toolbarIcon}>
+              <IconButton onClick={this.handleDrawerClose}>
+                <ChevronLeftIcon/>
+              </IconButton>
+            </div>
+            <Divider/>
+            <List>
+              <div>
+                <ListItem button onClick={() => this.setSelectedView("Home")}>
+                  <ListItemIcon>
+                    <DashboardIcon/>
+                  </ListItemIcon>
+                  <ListItemText primary="Home"/>
+                </ListItem>
+
+                <ListItem
+                  button
+                  onClick={() => this.setSelectedView("Businesses")}
+                >
+                  <ListItemIcon>
+                    <EventNoteIcon/>
+                  </ListItemIcon>
+                  <ListItemText primary="Businesses"/>
+                </ListItem>
+
+                <ListItem button onClick={() => this.setSelectedView("Users")}>
+                  <ListItemIcon>
+                    <PeopleIcon/>
+                  </ListItemIcon>
+                  <ListItemText primary="Users"/>
+                </ListItem>
+              </div>
+            </List>
+          </Drawer>
+          <main className={classes.content}>
+            <div className={classes.appBarSpacer}/>
+
+            {/* FIXME: Use proper routing here instead  */}
+            <Typography component="div" className={classes.chartContainer}>
+              {this.state.selectedView === "Home" ? children : null}
+              {this.state.selectedView === "Businesses" ? children : null}
+              {this.state.selectedView === "Users" ? children : null}
+            </Typography>
+          </main>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default withStyles(styles)(OriginalLayout);
