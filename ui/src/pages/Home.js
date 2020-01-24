85b52385ce0bdbd5e1f3d189d5d9e06af6fe4685
YAMADA Masaki <masaki@metabolics.co.jp>
2020-01-24 16:05:03 +0900

    とりあえず Home.js の中身を作ってみる
    行先はまだない

--- ui/src/pages/Home.js
+++ ui/src/pages/Home.js
@@ -1,9 +1,96 @@
 import React from "react";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import Grid from "@material-ui/core/Grid";
+import CardContent from "@material-ui/core/CardContent";
+import Card from "@material-ui/core/Card";
+import Typography from "@material-ui/core/Typography";
+import CardActions from "@material-ui/core/CardActions";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  card: {
+    minWidth: 275,
+  },
+  title: {
+    fontSize: 14,
+  }
+}));
+
+const cards = [
+  {
+    title: 'ひと',
+    cardinal: 1,
+    image: '',
+    newHandler: e => {
+      console.log(e)
+    },
+    listHandler: e => {
+      console.log(e)
+    },
+  },
+  {
+    title: 'セル',
+    cardinal: 2,
+    image: '',
+    newHandler: e => {
+      console.log(e)
+    },
+    listHandler: e => {
+      console.log(e)
+    },
+  },
+  {
+    title: '組織',
+    cardinal: 3,
+    image: '',
+    newHandler: e => {
+      console.log(e)
+    },
+    listHandler: e => {
+      console.log(e)
+    },
+  },
+  {
+    title: '市場',
+    cardinal: 4,
+    image: '',
+    newHandler: e => {
+      console.log(e)
+    },
+    listHandler: e => {
+      console.log(e)
+    },
+  }
+];
 
 export default () => {
+  const classes = useStyles();
+
   return (
-    <div>
-      Hi!
-    </div>
+    <Grid container className={classes.root} spacing={2}>
+      {
+        cards.map((card, index) =>
+          <Grid key={index} item xs={3}>
+            <Card className={classes.card}>
+              <CardContent>
+                <Typography className={classes.title} color="textSecondary" gutterBottom>
+                  {card.title}
+                </Typography>
+                <Typography>
+                  {card.cardinal}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small" onClick={card.newHandler}>New</Button>
+                <Button size="small" onClick={card.listHandler}>View</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        )
+      }
+    </Grid>
   )
 }
