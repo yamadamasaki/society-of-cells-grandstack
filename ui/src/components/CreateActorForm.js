86ac0eb4b076bc293ade46afad06890e999fec5b
YAMADA Masaki <masaki@metabolics.co.jp>
2020-02-17 19:55:30 +0900

    とりあえず CreateActor を formik から uniforms に移行する
    まずは AutoForm で実装する

--- ui/src/components/CreateActorForm.js
+++ ui/src/components/CreateActorForm.js
@@ -1,101 +1,60 @@
-import { Form, Formik, Field } from "formik";
-import { TextField, Select } from "formik-material-ui";
-import LinearProgress from "@material-ui/core/LinearProgress";
-import Button from "@material-ui/core/Button";
 import React from "react";
-import MenuItem from "@material-ui/core/MenuItem";
-import { CREATE_ACTOR, positions } from "../utils/model";
+import { CREATE_ACTOR } from "../utils/model";
 import { useMutation } from "@apollo/react-hooks";
 import { useSnackbar } from "notistack";
-import InputLabel from "@material-ui/core/InputLabel";
-import FormControl from "@material-ui/core/FormControl";
+import { AutoForm } from "uniforms-material";
+import Ajv from "ajv";
+import { JSONSchemaBridge } from "uniforms-bridge-json-schema";
 
-export default props => {
-  const onCancel = () => props.onClose(true, {});
+export default ({ onClose }) => {
   const [createActor] = useMutation(CREATE_ACTOR);
   const { enqueueSnackbar } = useSnackbar();
 
-  return (
-    <Formik
-      initialValues={{
-        name: "",
-        position: ""
-      }}
-      validate={values => {
-        const errors = {};
-        if (!values.name) errors.name = "Required";
-        if (!values.position) errors.position = "Required";
-        return errors;
-      }}
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          setSubmitting(false);
-          createActor({ variables: values }).then(r =>
-            enqueueSnackbar(
-              r.errors
-                ? `Failed to add an actor: ${r.errors}`
-                : `New actor added: ${r.data.CreateActor.name} (${r.data.CreateActor.id})`
-            )
-          );
-          props.onClose(true, values);
-        });
-      }}
-    >
-      {({ submitForm, isSubmitting }) => (
-        <Form>
-          <Field
-            component={TextField}
-            name="name"
-            type="text"
-            label="Name"
-            variant="filled"
-          />
-          <br />
-          <FormControl>
-            <InputLabel htmlFor="actor-position">Position</InputLabel>
-            <Field
-              component={Select}
-              name="position"
-              select={positions[0]}
-              variant="filled"
-              inputProps={{ id: "actor-position" }}
-            >
-              {positions.map((value, index) => (
-                <MenuItem key={index} value={value}>
-                  {value}
-                </MenuItem>
-              ))}
-            </Field>
-          </FormControl>
-          <br />
-          <Field
-            component={TextField}
-            name="qualification"
-            type="text"
-            label="Qualification"
-            variant="outlined"
-            multiline
-          />
-          <br />
-          <Field
-            component={TextField}
-            name="career"
-            type="text"
-            label="Career"
-            variant="outlined"
-            multiline
-          />
-          <br />
-          {isSubmitting && <LinearProgress />}
-          <br />
-          <Button color="primary" disabled={isSubmitting} onClick={submitForm}>
-            Submit
-          </Button>
-          <Button onClick={onCancel} color="primary">
-            Cancel
-          </Button>
-        </Form>
-      )}
-    </Formik>
-  );
+  const ajv = new Ajv({ allErrors: true, useDefaults: true });
+  const schema = {
+    title: "Actor",
+    type: "object",
+    properties: {
+      name: { type: "string" },
+      position: {
+        type: "string",
+        enum: [
+          "EXECUTIVE_FACILITATOR",
+          "PROJECT_FACILITATOR",
+          "ASSISTANT_FACILITATOR",
+          "PROFESSIONAL",
+          "EXPERT_STAFF",
+          "SENIOR_STAFF",
+          "ASSISTANT_STAFF",
+          "NEW_FACE"
+        ]
+      },
+      qualification: { type: "string" },
+      career: { type: "string" }
+    },
+    required: ["name", "position"]
+  };
+  const createValidator = schema => {
+    const validator = ajv.compile(schema);
+    return model => {
+      validator(model);
+
+      if (validator.errors && validator.errors.length)
+        throw { details: validator.errors };
+    };
+  };
+  const schemaValidator = createValidator(schema);
+  const bridge = new JSONSchemaBridge(schema, schemaValidator);
+  const onSubmit = model => {
+    createActor({ variables: model }).then(r => {
+      enqueueSnackbar(
+        r.errors
+          ? `Failed to add an actor: ${r.errors}`
+          : `New actor added: ${r.data.CreateActor.name} (${r.data.CreateActor.id})`
+      );
+      onClose();
+    });
+  };
+
+  return <AutoForm schema={bridge} onSubmit={onSubmit} />;
 };
