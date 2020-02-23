487b956d38502d94ba4edfc8cf4bf7998cf6d5c1
YAMADA Masaki <masaki@metabolics.co.jp>
2020-02-23 14:20:46 +0900

    CreateMarketForm を formik から uniforms+simple-schema2 に移行した
    これで, formik の残滓を拭い去ったはず

--- ui/src/components/CreateMarketForm.js
+++ ui/src/components/CreateMarketForm.js
@@ -1,47 +1,31 @@
-import {Form, Formik} from "formik";
-import {TextField} from "formik-material-ui";
-import {LinearProgress} from "@material-ui/core";
-import Button from "@material-ui/core/Button";
-import React from "react";
-import {CREATE_MARKET} from "../utils/model"
-import {useMutation} from "@apollo/react-hooks";
-import {useSnackbar} from "notistack";
+import React from 'react';
+import {CREATE_MARKET} from '../utils/model';
+import {useMutation} from '@apollo/react-hooks';
+import {useSnackbar} from 'notistack';
+import SimpleSchema from 'simpl-schema';
+import {AutoForm} from 'uniforms-material';
+import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
 
-export default props => {
-  const onCancel = () => props.onClose(true, {});
-  const [createMarket,] = useMutation(CREATE_MARKET);
+const schema = new SimpleSchema({
+  name: String,
+});
+
+export default ({onClose}) => {
+  const [createMarket] = useMutation(CREATE_MARKET);
   const {enqueueSnackbar} = useSnackbar();
 
+  const bridge = new SimpleSchema2Bridge(schema);
+  const onSubmit = model => {
+    createMarket({variables: model}).then(r => {
+      enqueueSnackbar(
+          r.errors ?
+              `Failed to add a market: ${r.errors}` :
+              `New market added: ${r.data.CreateMarket.name} (${r.data.CreateMarket.id})`,
+      );
+    });
+    onClose();
+  };
   return (
-    <Formik
-      initialValues={{
-        name: '',
-      }}
-      validate={values => {
-        const errors = {};
-        if (!values.name) errors.name = 'Required';
-        return errors
-      }}
-      onSubmit={(values, {setSubmitting}) => {
-        setTimeout(() => {
-          setSubmitting(false);
-          createMarket({variables: values})
-            .then(r => enqueueSnackbar(r.errors
-              ? `Failed to add a market: ${r.errors}`
-              : `New market added: ${r.data.CreateMarket.name} (${r.data.CreateMarket.id})`));
-          props.onClose(true, values)
-        })
-      }}>
-      {({submitForm, isSubmitting}) => (
-        <Form>
-          <TextField name="name" type="text" label="Name" variant="standard"/>
-          <br/>
-          {isSubmitting && <LinearProgress/>}
-          <br/>
-          <Button color="primary" disabled={isSubmitting} onClick={submitForm}>Submit</Button>
-          <Button onClick={onCancel} color="primary">Cancel</Button>
-        </Form>
-      )}
-    </Formik>
-  )
-}
+      <AutoForm schema={bridge} onSubmit={onSubmit}/>
+  );
+};
